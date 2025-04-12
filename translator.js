eed8fc021f591546190ae92f4793aa9139fd3939
Lina-Anfinogenova <AnfinogenovaLina@yandex.ru>
2025-04-12 13:24:14 +0600

    Шутер

---
+++ translator.js
@@ -0,0 +1,20 @@
+{
+    "ru": {
+        "winner": "Победа!",
+        "looser": "Проигрыш",
+        "comment": "Нажмите Esc, чтобы начать заново",
+        "score": "Счёт",
+        "skipped": "Пропущено",
+        "recharge": "Перезарядка",
+        "sec": "сек."
+    },
+    "en": {
+        "winner": "YOU WIN!",
+        "looser": "YOU LOSE!",
+        "comment": "Press Esc to start",
+        "score": "Score",
+        "skipped": "Skipped",
+        "recharge": "Recharge",
+        "sec": "sec."
+    }
+}
